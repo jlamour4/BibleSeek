2758b0fb5eb27efcdb112982be03e19328559d2c
jlamour4 <jlamour4@gmail.com>
2024-09-18 21:07:41 -0400

    Adding updates to UI components

--- BibleSeek/app/components/ScriptureCard.js
+++ BibleSeek/app/components/ScriptureCard.js
@@ -1,42 +1,102 @@
-import React from "react";
-import { StyleSheet, View, Text, Pressable, SafeAreaView } from "react-native";
+import React from 'react';
+import {StyleSheet, View, Text, Pressable, SafeAreaView} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { useNavigation } from "@react-navigation/native";
+import {useNavigation} from '@react-navigation/native';
 
-
-const ScriptureCard = (props) => {
+const ScriptureCard = props => {
   const navigation = useNavigation();
   const votes = props.data.votes;
   const verse = props.data.verse;
   const verseLocation = props.data.verseLocation;
+
+  voteFunction((topic, verseItem, voteValue) => {
+    verseId = verseItem.startVerseId + verseItem.endVerseId;
+
+    let formdata = new FormData();
+
+    formdata.append('word', topic);
+    formdata.append('db', 'dy');
+    formdata.append('id', 'vote_' + voteValue + '_' + verseId);
+    fetch('https://www.openbible.info/topics/tools/vote?w=' + topic, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: formdata,
+    })
+      .then(response => {
+        console.log('Vote successful');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
   return (
     <SafeAreaView style={styles.container}>
-
       <View style={styles.cardHeader}>
         <Text style={styles.verseLocation}>{verseLocation}</Text>
-        <MaterialCommunityIcons name="dots-vertical" color="black" size={25} style={{padding:1}} onPress={() => { alert("MORE"); }}/>
+        <MaterialCommunityIcons
+          name="dots-vertical"
+          color="black"
+          size={25}
+          style={{padding: 1}}
+          onPress={() => {
+            alert('MORE');
+          }}
+        />
       </View>
       <View style={styles.cardBody}>
-        <Text numberOfLines={7} textBreakStrategy="highQuality">{verse}</Text>
+        <Text numberOfLines={7} textBreakStrategy="highQuality">
+          {verse}
+        </Text>
       </View>
       <View style={styles.cardFooter}>
-        <View style={{ flexDirection: "row", alignItems: "center"}}>
-
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
           <View style={styles.upvoteContainer}>
-          <Pressable style={styles.upvoteButton} onPress={() => { alert("up"); }} android_ripple={{color: 'grey', borderless: true}}>
-                <MaterialCommunityIcons name="thumb-up-outline" color={global.grey} size={25}/>
-                <Text>{votes}</Text>
+            <Pressable
+              style={styles.upvoteButton}
+              onPress={() => {
+                alert('up');
+              }}
+              android_ripple={{color: 'grey', borderless: true}}>
+              <MaterialCommunityIcons
+                name="thumb-up-outline"
+                color={global.grey}
+                size={25}
+              />
+              <Text>{votes}</Text>
             </Pressable>
           </View>
-          
+
           <View style={styles.downvoteContainer}>
-            <Pressable style={styles.downvoteButton} onPress={() => { alert("down"); }} android_ripple={{color: 'grey', borderless: true}}>
-              <MaterialCommunityIcons name="thumb-down-outline" color={global.grey} size={25}/>       
+            <Pressable
+              style={styles.downvoteButton}
+              onPress={() => {
+                alert('down');
+              }}
+              android_ripple={{color: 'grey', borderless: true}}>
+              <MaterialCommunityIcons
+                name="thumb-down-outline"
+                color={global.grey}
+                size={25}
+              />
             </Pressable>
           </View>
         </View>
-        <Text style={{fontSize: 16, opacity: .75}}>{votes} helpful {votes == 1 ? "vote" : "votes"}</Text>
-        <MaterialCommunityIcons name="heart" color={global.grey} size={25} style={{padding:1}} onPress={() => { alert("love!"); }}/>
+        <Text style={{fontSize: 16, opacity: 0.75}}>
+          {votes} helpful {votes == 1 ? 'vote' : 'votes'}
+        </Text>
+        <MaterialCommunityIcons
+          name="heart"
+          color={global.grey}
+          size={25}
+          style={{padding: 1}}
+          onPress={() => {
+            alert('love!');
+          }}
+        />
       </View>
     </SafeAreaView>
   );
@@ -48,64 +108,63 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 5,
     marginBottom: 5,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     padding: 10,
-    width: "100%",
+    width: '100%',
   },
   cardHeader: {
-    justifyContent: "space-between",
-    flexDirection: "row",
+    justifyContent: 'space-between',
+    flexDirection: 'row',
   },
   verseLocation: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     fontSize: 18,
-    color: global.darkGrey
+    color: global.darkGrey,
   },
   cardBody: {
-    marginBottom: 20
+    marginBottom: 20,
   },
   cardFooter: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   flexRow: {
-    flexDirection: "row",
-    alignContent: "center"
+    flexDirection: 'row',
+    alignContent: 'center',
   },
   input: {
     fontSize: 18,
     marginLeft: 10,
-    width: "90%",
-    flexWrap: "nowrap"
+    width: '90%',
+    flexWrap: 'nowrap',
   },
   upvoteContainer: {
-    alignSelf: "center",
+    alignSelf: 'center',
     backgroundColor: global.offwhite,
     borderBottomLeftRadius: 20,
     borderTopLeftRadius: 20,
     borderRightWidth: 1,
     borderRightColor: global.white,
-    overflow: 'hidden'
+    overflow: 'hidden',
   },
   downvoteContainer: {
-    alignSelf: "center",
+    alignSelf: 'center',
     backgroundColor: global.offwhite,
     borderBottomRightRadius: 20,
     borderTopRightRadius: 20,
     borderLeftWidth: 1,
     borderLeftColor: global.white,
-    overflow: 'hidden'
+    overflow: 'hidden',
   },
   upvoteButton: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 10
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
   },
   downvoteButton: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 10
-  }
-
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
 });
--- BibleSeek/app/components/TopicResultsList.js
+++ BibleSeek/app/components/TopicResultsList.js
@@ -1,6 +1,6 @@
 // TopicResultsList.js
-import React, { useState, useRef, useEffect } from "react";
-import { useNavigation } from "@react-navigation/native";
+import React, {useState, useRef, useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
 import {
   StyleSheet,
   Text,
@@ -8,89 +8,83 @@ import {
   FlatList,
   SafeAreaView,
   ActivityIndicator,
-  Animated
-} from "react-native";
+  Animated,
+} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import ScriptureCard from "./ScriptureCard";
-import SearchBar from "./SearchBar";
+import ScriptureCard from './ScriptureCard';
+import SearchBar from './SearchBar';
 
-
-const Item = ({ verseData }) => (
-  <ScriptureCard style={styles.scriptureCard} data={verseData} />
+const Item = ({verseData, topic}) => (
+  <ScriptureCard style={styles.scriptureCard} data={verseData} topic={topic} />
 );
 
-const TopicResultsList = (props) => {
-  console.log("WAIT");
+const TopicResultsList = props => {
   const navigation = useNavigation();
-  const item = props.topicSelected;
-  console.log("BING BONG");
-  console.log(item);
+  const topic = props.topicSelected;
   const [topicResults, setResults] = useState();
   const [versesCount, setVersesCount] = useState(0);
   const t_kjv = require('../assets/lib/t_kjv.json');
   const bible_key = require('../assets/lib/bible_key_english.json');
   const jsonBible = t_kjv.resultset.row;
   const bibleKey = bible_key.resultset.keys;
-  const fadeAnim = useRef(new Animated.Value(0)).current // Initial value for opacity: 0
-  
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
   // get data from the fake api
   useEffect(() => {
     const getData = async () => {
       const apiResponse = await fetch(
-        "https://bibleseek-3ade5-default-rtdb.firebaseio.com/topics/" + item + ".json"
-      );      
+        'https://bibleseek-3ade5-default-rtdb.firebaseio.com/topics/' +
+          topic +
+          '.json',
+      );
       const data = await apiResponse.json();
       setResults(data);
       setVersesCount(data.length);
     };
     getData();
-    Animated.timing(
-      fadeAnim,
-      {
-        toValue: 1,
-        duration: 1250,
-        useNativeDriver: true,
-      }
-    ).start();
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1250,
+      useNativeDriver: true,
+    }).start();
   }, [fadeAnim]);
 
   function getVerses(startVerseId, endVerseId) {
-    let verseString = "";
-    for (var i = 0; i < jsonBible.length; i++ ) {
+    let verseString = '';
+    for (var i = 0; i < jsonBible.length; i++) {
       let el = jsonBible[i];
       if (el.field[0] >= startVerseId) {
         verseString += el.field[4];
         if (!endVerseId || el.field[0] > endVerseId) break;
-        verseString += " ";
+        verseString += ' ';
       }
     }
     return verseString;
   }
 
   function getVerseLocation(startVerseId, endVerseId) {
-    // 64 001 011
-    let book = bibleKey[parseInt(startVerseId.toString().slice(0,2))-1].n;
-    let startChapter = startVerseId.toString().slice(2,5).replaceAll('0','');
-    let startVerse = startVerseId.toString().slice(5).replaceAll('0','');
+    let book = bibleKey[parseInt(startVerseId.toString().slice(0, 2)) - 1].n;
+    let startChapter = startVerseId.toString().slice(2, 5).replaceAll('0', '');
+    let startVerse = startVerseId.toString().slice(5).replaceAll('0', '');
 
-    let verseLocation = book + " " + startChapter + ":" + startVerse
+    let verseLocation = book + ' ' + startChapter + ':' + startVerse;
 
     if (endVerseId) {
-      let endVerse = endVerseId.toString().slice(5).replaceAll('0','');
-      verseLocation += "-" + endVerse;
+      let endVerse = endVerseId.toString().slice(5).replaceAll('0', '');
+      verseLocation += '-' + endVerse;
     }
 
     return verseLocation;
   }
 
-  const renderItem = ({ item }) => {
+  const renderItem = ({item, topic}) => {
     // item.endVerseId
     // item.startVerseId
     item.verseLocation = getVerseLocation(item.startVerseId, item.endVerseId);
     item.verse = getVerses(item.startVerseId, item.endVerseId);
     return (
       // <Pressable style={styles.topicPressable} onPress={() => { navigation.navigate('TopicResult', item)}}  android_ripple={{borderless: false}}>
-        <Item verseData={item} />
+      <Item verseData={item} topic={topic} />
       // </Pressable>
     );
   };
@@ -116,31 +110,36 @@ export default TopicResultsList;
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
     backgroundColor: global.offwhite,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'cetner',
+    alignItems: 'center',
   },
   topBar: {
     paddingLeft: 25,
     paddingRight: 25,
-    flexDirection: "row",
-    width: "100%",
-    borderBottomColor: "#9da29f",
+    flexDirection: 'row',
+    width: '100%',
+    borderBottomColor: '#9da29f',
     borderBottomWidth: 1,
-    alignItems: "center",
+    alignItems: 'center',
   },
   header: {
     fontSize: 18,
     marginLeft: 10,
-    width: "90%",
-    flexWrap: "wrap"
+    width: '90%',
+    flexWrap: 'wrap',
   },
   bold: {
-    fontWeight: "bold"
+    fontWeight: 'bold',
   },
   activityIndicator: {
     transform: [{scaleX: 1.3}, {scaleY: 1.3}],
-    height: "100%",
-    alignSelf: "center",
-    flexDirection: "row",
-  }
+    height: '100%',
+    alignSelf: 'center',
+    flexDirection: 'row',
+  },
 });
